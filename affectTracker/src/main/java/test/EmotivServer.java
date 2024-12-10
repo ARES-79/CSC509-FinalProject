71cda19687cc48bcef232f4085e1ce30db03915c
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2024-12-10 11:32:01 -0800

    Setup emotiv server sample code

---
+++ affectTracker/src/main/java/test/EmotivServer.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.net.URI;
+
+/**
+ * Main class to run the Emotiv WebSocket client.
+ *
+ *  @author javiersgs
+ *  @version 0.1
+ */
+public class EmotivServer {
+
+    public static void main(String[] args) throws Exception {
+        EmotivDelegate delegate = new EmotivDelegate();
+        URI uri = new URI("wss://localhost:6868");
+        EmotivSocket ws = new EmotivSocket(uri, delegate);
+        ws.connect();
+    }
+}
